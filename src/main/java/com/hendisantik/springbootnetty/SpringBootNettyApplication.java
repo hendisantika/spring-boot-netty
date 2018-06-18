aa78013ae0b0c11a102173d8426fec38c65be2df
Hendi Santika <hendisantika@gmail.com>
2018-06-19 05:46:35 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantik/springbootnetty/SpringBootNettyApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantik.springbootnetty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNettyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootNettyApplication.class, args);
+    }
+}
